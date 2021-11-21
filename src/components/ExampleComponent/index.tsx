691aa3aca6dbddaddb40a4d8d43c0b8b41938b86
nus3 <hadayota33@gmail.com>
2021-11-21 19:40:10 +0900

    :art: Add example component story

--- src/components/ExampleComponent/index.tsx
+++ src/components/ExampleComponent/index.tsx
@@ -1,7 +1,37 @@
-import { VFC } from 'react'
+import { useState, VFC } from 'react'
 
 import styles from './styles.module.css'
 
 export const ExampleComponent: VFC = () => {
-  return <button className={styles.btn}>ボタンだよ</button>
+  const [rows, setRows] = useState<string[]>([])
+
+  const handleClickAdd = () => {
+    const r = [...rows]
+    r.push('')
+    setRows(r)
+  }
+
+  const handleClickReset = () => {
+    setRows([])
+  }
+
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.btnWrap}>
+        <button className={styles.btn} onClick={handleClickAdd}>
+          Add
+        </button>
+        <button className={styles.btn} onClick={handleClickReset}>
+          Reset
+        </button>
+      </div>
+      <ul className={styles.list}>
+        {rows.map((row, index) => (
+          <li key={index} className={styles.item}>
+            {`${index + 1}番目のコンテンツ`}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from 'styles/Home.module.css'
+import 'styles/normalize.css'
 
 const Home: NextPage = () => {
   return (
