af76a4b738b1b60acb6e01452505e1e4fea09ce0
nus3 <hadayota33@gmail.com>
2022-02-28 23:09:27 +0900

    :+1: Add no.2 of css trace course

--- src/components/Egghead/index.tsx
+++ src/components/Egghead/index.tsx
@@ -2,17 +2,29 @@
 import { VFC } from 'react'
 import { createPublicPath } from 'utils/path'
 
-import styles from './styles.module.css'
+import './styles.css'
 
 export const Egghead: VFC = () => {
   return (
-    <div className={styles.wrap}>
+    <div className="wrap">
       <img
-        className={styles.img}
+        className="img"
         src={createPublicPath(`/images/egghead.png`)}
         alt={`egghead icon`}
       />
-      <div className={styles.egg} />
+      <div className="egg">
+        <div className="specs__arms" />
+        <div className="egg_shell shell">
+          <div className="shell__piece shell__piece--top" />
+          <div className="shell__piece shell__piece--middle" />
+          <div className="shell__piece shell__piece--bottom" />
+        </div>
+        <div className="egg_specs specs">
+          <div className="specs_lens specs__lens--left" />
+          <div className="specs_lens specs__lens--right" />
+          <div className="specs_bridge" />
+        </div>
+      </div>
     </div>
   )
 }
