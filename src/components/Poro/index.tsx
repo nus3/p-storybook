85d5c54ba31d0532a2eb809e009400330368c66f
nus3 <hadayota33@gmail.com>
2022-05-08 15:32:50 +0900

    :+1: Add top part

--- src/components/Poro/index.tsx
+++ src/components/Poro/index.tsx
@@ -10,13 +10,15 @@ export const Poro: VFC = () => {
       <img src={createPublicPath(`/images/poro.png`)} alt={`poro icon`} />
       <div className="poro">
         <div className="poro-body">
-          <div className="poro-body_base poro-body__top" />
+          <div className="poro-body_base poro-body__top-right" />
           <div className="poro-body_base poro-body__top-left" />
+          <div className="poro-body_base poro-body__center" />
+          <div className="poro-body_base poro-body__center-left" />
           <div className="poro-body_base poro-body__left-hand" />
           <div className="poro-body_base poro-body__bottom-left" />
           <div className="poro-body_base poro-body__left-foot" />
           <div className="poro-body_base poro-body__bottom" />
-          <div className="poro-body_base poro-body__top-right" />
+          <div className="poro-body_base poro-body__center-right" />
           <div className="poro-body_base poro-body__right-hand" />
           <div className="poro-body_base poro-body__right-foot" />
         </div>
